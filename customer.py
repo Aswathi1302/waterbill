e0f5fb6dddf98eb14c4ab3e6a4890132293f813d
Aswathi1302 <aswathisomaraj1302@gmail.com>
2022-11-17 17:27:15 +0530

    first commit

---
+++ customer.py
@@ -0,0 +1,36 @@
+while True:
+    print("SELECT A OPTION")
+    print("1.ADD CUSTOMER")
+    print("2.VIEW CUSTOMER")
+    print("3.SEARCH CUSTOMER")
+    print("4.DELETE CUSTOMER")
+    print("5.UPDATE CUSTOMER")
+    print("6.GENERAT BILL")
+    print("")
+    print("")
+    print("9.EXIT")
+    choice=int(input("select a option:-- "))
+    if(choice==1):
+        print("1.ADD CUSTOMER")
+        code=input("enter the code:-")
+        name=input("enter the name:-")
+        place=input("enter the place:-")
+        phone=input("enter the phone:-")
+        email=input("enter the email:-")
+    elif(choice==2):
+        print("2.VIEW CUSTOMER")
+    elif(choice==3):
+        print("3.SEARCH CUSTOMER")
+    elif(choice==4):
+        print("4.DELETE CUSTOMER")
+    elif(choice==5):
+        print("5.UPDATE CUSTOMER")
+    elif(choice==6):
+        print("6.GENERAT BILL")
+    elif(choice==9):
+        break
+
+
+
+
+            
